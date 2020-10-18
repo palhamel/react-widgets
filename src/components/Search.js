c78a65bfa0d5abe9fcc59000cf3d99a9ab263a37
Pål Hamel <pal.hamel@gmail.com>
2020-10-18 18:53:42 +0200

    add results array and init search string

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
-  console.log(term);
+  const [term, setTerm] = useState('greta thunberg');
+  // console.log(term);
+  const [results, setResults] = useState([]);
+  // console.log(results);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
@@ -16,6 +18,7 @@ const Search = () => {
           srsearch: term,
         },
       });
+      setResults(data.query.search);
     };
     search();
   }, [term]);
