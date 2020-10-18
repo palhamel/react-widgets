a825ab19202b0ed04145450f6685f0bef88bf6a5
Pål Hamel <pal.hamel@gmail.com>
2020-10-18 15:49:38 +0200

    add useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  console.log(term);
+
+  useEffect(() => {
+    console.log('use effect running');
+  }, [term])
 
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
-          <input type="text" className="input" />
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
       </div>
     </div>
