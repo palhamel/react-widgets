de962f2604932121b0ef785ced45d756f889ddff
Pål Hamel <pal.hamel@gmail.com>
2020-10-18 15:09:31 +0200

    start on search component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from "react";
-import Accordion from "./components/Accordion";
-
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a JS framework. And it's pretty fun to use.",
-  },
-  {
-    title: "What is JSX?",
-    content:
-      "JSX is a syntax extension to JavaScript and comes with the full power of JavaScript..",
-  },
-  {
-    title:
-      "What are the differences between a class component and functional component?",
-    content:
-      "Class components allows us to use additional features such as local state and lifecycle hooks. Also, to enable our component to have direct access to our store and thus holds state.",
-  },
-];
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a JS framework. And it's pretty fun to use.",
+//   },
+//   {
+//     title: "What is JSX?",
+//     content:
+//       "JSX is a syntax extension to JavaScript and comes with the full power of JavaScript..",
+//   },
+//   {
+//     title:
+//       "What are the differences between a class component and functional component?",
+//     content:
+//       "Class components allows us to use additional features such as local state and lifecycle hooks. Also, to enable our component to have direct access to our store and thus holds state.",
+//   },
+// ];
 
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
