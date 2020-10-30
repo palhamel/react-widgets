7c60fec4f45fc5437afa4605277eea609960e2ae
Pål Hamel <pal.hamel@gmail.com>
2020-10-30 10:59:36 +0100

    setup state in app component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -18,13 +18,11 @@ const Dropdown = ({ options }) => {
         <div className="ui selection dropdown visible active">
           <i className="dropdown icon"></i>
           <div className="text">Drop down here</div>
-          <div className="menu visible transition">
-            {renderedOptions}
-          </div>
+          <div className="menu visible transition">{renderedOptions}</div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Dropdown;
