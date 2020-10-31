5706204593d357e05d602342a63eb244c19ae77c
Pål Hamel <pal.hamel@gmail.com>
2020-10-31 15:37:25 +0100

    setup axios

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Convert = ({ language, text}) => {
+  const apiUrl = 'https://translation.googleapis.com/language/translate/v2';
 
   useEffect(() => {
-    console.log('new lang or text');
+    axios.post(apiUrl, {}, {
+      params: {
+        q: text,
+        target: language.value,
+        key: process.env.REACT_APP_GOOGLE_API_KEY
+      }
+    })
   }, [language, text])
 
 
